0df5c4ddabb227ee3083ccf146538bcc2d431f74
Samantha Lansoy <sdlansoy@up.edu.ph>
2026-04-05 19:54:24 +0800

    feat: changed layout of add org modal and added cover photo

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 import './bootstrap';
+
+function previewImage(input, previewId) {
+    const preview = document.getElementById(previewId);
+    if (input.files && input.files[0]) {
+        const reader = new FileReader();
+        reader.onload = e => {
+            preview.src = e.target.result;
+            preview.classList.remove('hidden');
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+window.previewImage = previewImage;
